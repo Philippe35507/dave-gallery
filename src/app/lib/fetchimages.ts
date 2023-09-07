4bbb52307dcfda044e907dd622ea7f67c6249a8f
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-09-07 10:36:17 +0100

    CleanEnv + str from Envalid

--- src/app/lib/fetchimages.ts
+++ src/app/lib/fetchimages.ts
@@ -1,19 +1,20 @@
 import type { ImagesResults } from "@/models/Images";
 import { ImagesSchemaWithPhotos } from "@/models/Images";
+import env from "./env";
 
 export default async function fetchImages(
   url: string
 ): Promise<ImagesResults | undefined> {
   try {
     const res = await fetch(url, {
-      headers: { authorization: process.env.PEXELS_API_KEY },
+      headers: { authorization: env.PEXELS_API_KEY },
     });
 
     if (!res.ok) throw new Error("Fetch Images error!\n");
 
     const imagesResults: ImagesResults = await res.json();
 
-    console.log(imagesResults);
+    // console.log(imagesResults);
 
     // Parse data with Zod schema
     const parsedData = ImagesSchemaWithPhotos.parse(imagesResults);
