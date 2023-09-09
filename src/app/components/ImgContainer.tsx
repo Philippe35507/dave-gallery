7b52b5724e1072582b16825473755c4d94b03c7d
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-09-09 21:09:18 +0100

    Blurred Images as Placeholders

--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -7,16 +7,15 @@ type Props = {
 
 export default function ImgContainer({ photo }: Props) {
   return (
-    <div
-      key={photo.id}
-      className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group"
-    >
+    <div className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group">
       <Image
         src={photo.src.large}
         alt={photo.alt}
         fill={true}
         sizes="(min-width: 1300px) 282px, (min-width: 1040px) calc(11.67vw + 133px), (min-width: 780px) 33.33vw, (min-width: 520px) 50vw, 100vw"
         className="object-cover group-hover:opacity-75"
+        placeholder="blur"
+        blurDataURL={photo.blurredDataUrl}
       ></Image>
     </div>
   );
