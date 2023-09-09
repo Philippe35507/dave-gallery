c66a6bd5aea402f118e6042b6f5dc02b46f705e7
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-09-09 20:26:56 +0100

    Changing Image Opacity on Mouse Over

--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -9,14 +9,14 @@ export default function ImgContainer({ photo }: Props) {
   return (
     <div
       key={photo.id}
-      className="h-64 bg-gray-200 rounded-xl relative overflow-hidden"
+      className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group"
     >
       <Image
         src={photo.src.large}
         alt={photo.alt}
         fill={true}
         sizes="(min-width: 1300px) 282px, (min-width: 1040px) calc(11.67vw + 133px), (min-width: 780px) 33.33vw, (min-width: 520px) 50vw, 100vw"
-        className="object-cover"
+        className="object-cover group-hover:opacity-75"
       ></Image>
     </div>
   );
