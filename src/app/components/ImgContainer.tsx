48804ebac49b5c58c86c8b9c41c5a9716d94ad72
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-09-09 20:23:12 +0100

    Responsive Image Optimization
    Using respimagelint

--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -7,12 +7,16 @@ type Props = {
 
 export default function ImgContainer({ photo }: Props) {
   return (
-    <div key={photo.id} className="h-64 bg-gray-200 rounded-xl">
+    <div
+      key={photo.id}
+      className="h-64 bg-gray-200 rounded-xl relative overflow-hidden"
+    >
       <Image
         src={photo.src.large}
         alt={photo.alt}
-        width={250}
-        height={250}
+        fill={true}
+        sizes="(min-width: 1300px) 282px, (min-width: 1040px) calc(11.67vw + 133px), (min-width: 780px) 33.33vw, (min-width: 520px) 50vw, 100vw"
+        className="object-cover"
       ></Image>
     </div>
   );
